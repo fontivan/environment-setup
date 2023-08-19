259d129d7a2ee991ebcd5cb40b30049a2ad46268
Steven Skeard <stevenskeard@gmail.com>
2023-08-19 00:31:32 -0230

    Add protonup flatpak

--- src/bin/common/08-flatpak.sh
+++ src/bin/common/08-flatpak.sh
@@ -7,9 +7,10 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sudo flatpak update -y
 
 # Install flatpaks
-sudo flatpak install flathub \
+sudo flatpak install -y flathub \
     "com.axosoft.GitKraken" \
     "com.discordapp.Discord" \
     "com.jetbrains.IntelliJ-IDEA-Community" \
     "com.spotify.Client" \
+    "net.davidotek.pupgui2" \
     "net.lutris.Lutris"
