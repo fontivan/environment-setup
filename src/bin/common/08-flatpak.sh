3b3fd1ca35180fd6a04a3483341f7b9ffc61e468
Steven Skeard <stevenskeard@gmail.com>
2023-08-20 10:57:49 -0400

    Remove gitkraken, intellij

--- src/bin/common/08-flatpak.sh
+++ src/bin/common/08-flatpak.sh
@@ -8,10 +8,8 @@ sudo flatpak update -y
 
 # Install flatpaks
 sudo flatpak install -y flathub \
-    "com.axosoft.GitKraken" \
     "com.discordapp.Discord" \
     "com.github.tchx84.Flatseal" \
-    "com.jetbrains.IntelliJ-IDEA-Community" \
     "com.spotify.Client" \
     "net.davidotek.pupgui2" \
     "net.lutris.Lutris" \
