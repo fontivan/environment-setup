d5c0edafc5ef8656475f7e5511125d1873839f93
Steven Skeard <stevenskeard@gmail.com>
2024-12-27 11:19:07 -0330

    Fix generating yum repos inadvertently replacing special characters

--- src/bin/fedora/04-insync-repo.sh
+++ src/bin/fedora/04-insync-repo.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Install insync rpm key
 sudo rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
 
 # Install insync repo
-sudo bash -c 'echo """
-[insync]
-name=insync repo
-baseurl=http://yum.insync.io/fedora/$releasever/
-gpgcheck=1
-gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
-enabled=1
-metadata_expire=120m
-""" > /etc/yum.repos.d/insync.repo'
+sudo cp "${SCRIPT_DIR}/../../etc/yum/insync.repo" "/etc/yum.repos.d/insync.repo"
