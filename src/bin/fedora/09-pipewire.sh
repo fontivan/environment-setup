dc9481e13463a439ff184ae00ec94966d620e0e1
Steven Skeard <stevenskeard@gmail.com>
2025-02-11 22:12:37 -0500

    Update pipewire configuration to customize more things, mostly to prevent audio cracking due to quantum configuration

--- src/bin/fedora/09-pipewire.sh
+++ src/bin/fedora/09-pipewire.sh
@@ -3,12 +3,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install pipewire configuration into the conf.d folder and restart pipewire service
-PIPEWIRE_CONF_DIR="${HOME}/.config/pipewire/pipewire.conf.d/"
-DENOISING_FILE="99-input-denoising.conf"
+PIPEWIRE_CONF_DIR="${HOME}/.config/pipewire/"
 
 mkdir -p "${PIPEWIRE_CONF_DIR}"
-cp "${SCRIPT_DIR}/../../etc/pipewire/${DENOISING_FILE}" \
-    "${PIPEWIRE_CONF_DIR}/${DENOISING_FILE}"
+cp -R ${SCRIPT_DIR}/../../etc/pipewire/* \
+    "${PIPEWIRE_CONF_DIR}/"
 
 systemctl daemon-reload --user
 systemctl restart --user pipewire.service
