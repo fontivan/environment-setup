9e12ccc7015984cf21262cd58e70a4d69d883dbe
Steven Skeard <stevenskeard@gmail.com>
2026-05-17 11:34:50 -0400

    Update Wireplumber conf to copy entire contents of folder instead of a single file

--- src/bin/fedora/10-wireplumber.sh
+++ src/bin/fedora/10-wireplumber.sh
@@ -4,11 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Install wireplumber configuration into the conf.d folder and restart wireplumber service
 WP_CONFIG_DIR="${HOME}/.config/wireplumber/wireplumber.conf.d/"
-WP_FILE="51-disable-suspension.conf"
 
 mkdir -p "${WP_CONFIG_DIR}"
-cp "${SCRIPT_DIR}/../../etc/wireplumber/${WP_FILE}" \
-    "${WP_CONFIG_DIR}/${WP_FILE}"
+cp -R "${SCRIPT_DIR}/../../etc/wireplumber/"* \
+    "${WP_CONFIG_DIR}/"
 
 systemctl daemon-reload --user
 systemctl restart --user wireplumber.service
